ac25fcf206d689372a8de3fb8a1a1c21b18e4c91
ryhem-belhoula <belhoularyhem7@gmail.com>
2025-04-29 20:49:43 +0100

    mod2

---
+++ module2/spring-thymeleaf-lab/src/test/java/com/isett/springthymeleaflab/SpringThymeleafLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isett.springthymeleaflab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
